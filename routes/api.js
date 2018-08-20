591d873001d3624050e676d1f4afcb41d550f433
LSD <renwfy@126.com>
2018-08-20 16:47:52 +0800

    修复接口

--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,21 @@
  * api路由
  * @param app
  */
+var config = require('../config');
+var apiPrefix = '/api/' + config.API_VISION;
+
 module.exports = function (app) {
     //lesson
     let lesson = require('./api/lesson');
-    app.get('/api/lessons', lesson.lessonList);
-    app.get('/api/lessons/activity_stats', lesson.activityStats);
-    app.post('/api/lessons/like', lesson.likePOST);
-    app.get('/api/lessons/like', lesson.likeGET);
+    app.get(apiPrefix + '/lessons', lesson.lessonList);
+    app.get(apiPrefix + '/lessons/activity_stats', lesson.activityStats);
+    app.post(apiPrefix + '/lessons/like', lesson.likePOST);
+    app.get(apiPrefix + '/lessons/like', lesson.likeGET);
 
 
     //user
     let user = require('./api/user');
-    app.post('/api/user', user.login0regist);
-    app.get('/api/user/info', user.userInfo);
-    app.post('/api/user/update', user.update);
+    app.post(apiPrefix + '/user', user.login0regist);
+    app.get(apiPrefix + '/user/info', user.userInfo);
+    app.post(apiPrefix + '/user/update', user.update);
 };
